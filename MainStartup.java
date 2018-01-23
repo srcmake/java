c26969c1f3c9f10a2b90b92657221883540893b2
srcmake <github@srcmake.com>
2018-01-23 00:56:56 -0500

    Video 4 - Functions

--- MainStartup.java
+++ MainStartup.java
@@ -1,30 +1,43 @@
 class MainStartup
         {
+        public static void SayHi(String name)
+        	{
+        	System.out.println("Hi from " + name);
+        	}
+        
+        public static int ReturnsFive()
+        	{
+        	int five = 5;
+        	return five;
+        	}
+        	
+		public static boolean IsEven(int num)
+			{
+			if(num % 2 == 0) // Means the number is even.
+				{ 
+				return true; 
+				}
+			
+			return false;
+			}
+        
         public static void main(String[] args)      
                 {
                 System.out.println("Program began.");
                 
-            	int max = 5;
-            	
-            	for(int i = 0; i < max; i++)
-            		{
-					System.out.print(i + " ");
-            		}
-            	System.out.println(".");
-            	
-            	
-            	if(max > 4) 
-            		{
-            		System.out.println("The number stored in \"max\" is greater than 4.");
-            		}
-            	else if (max > 2) 
-            		{
-            		System.out.println("The number stored in \"max\" is greater than 2 but less than 4.");
-            		}
-            	else 
-            		{
-            		System.out.println("The number stored in \"max\" is less than 2.");
-            		}
+                SayHi("srcmake");
                 
+                int num = ReturnsFive(); // num = 5
+                
+                if(IsEven(num) == true)
+                	{
+                	System.out.println(num + " is even!");
+                	}
+            	else
+            		{
+            		System.out.println(num + " is odd!");
+            		}
+            	
+                System.out.println("Program ended.");
                 }
         }
