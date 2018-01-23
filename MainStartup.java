a314d217c2702c9040b40cd5bed0d614b5ac2156
srcmake <github@srcmake.com>
2018-01-23 00:30:51 -0500

    Video 3 - For Loops, if statement

--- MainStartup.java
+++ MainStartup.java
@@ -2,20 +2,29 @@ class MainStartup
         {
         public static void main(String[] args)      
                 {
-                System.out.println("Variables, basic data types.");
+                System.out.println("Program began.");
                 
-                int count;
-                int subscribers = 100;
-                subscribers = -100;
-                long subs = 10000000L;
-                double decimal = 120.4; 
-                final int constant = 1;
-				
-				boolean mySubscribersAreCool = true; // false
-				
-				char c = 'A'; // ASCII
-                String warning = "Always compile, to make sure the code works.";
+            	int max = 5;
+            	
+            	for(int i = 0; i < max; i++)
+            		{
+					System.out.print(i + " ");
+            		}
+            	System.out.println(".");
+            	
+            	
+            	if(max > 4) 
+            		{
+            		System.out.println("The number stored in \"max\" is greater than 4.");
+            		}
+            	else if (max > 2) 
+            		{
+            		System.out.println("The number stored in \"max\" is greater than 2 but less than 4.");
+            		}
+            	else 
+            		{
+            		System.out.println("The number stored in \"max\" is less than 2.");
+            		}
                 
-                System.out.println(warning);
                 }
         }
