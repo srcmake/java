113991d0a0a47045b3839fe597c43f080fa9e4d4
srcmake <github@srcmake.com>
2018-02-01 15:21:38 -0500

    Video A - Maven project

---
+++ maven/ExampleBuild/src/main/java/com/srcmake/builder/App.java
@@ -0,0 +1,13 @@
+package com.srcmake.builder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from srcmake!" );
+    }
+}
